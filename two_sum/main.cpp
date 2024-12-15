0129a25425f83fe81412c22d5052b3f1b5a927ae
Andre Mochammad Satrio <79563756+amsatrio@users.noreply.github.com>
2024-12-15 09:40:09 +0700

    add two_sum test logic

--- two_sum/main.cpp
+++ two_sum/main.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 #include <vector>
 using namespace std;
 
@@ -28,3 +29,36 @@ public:
     }
 };
 
+int main(){
+    // get data
+    int t;
+    vector<vector<int>> vecs;
+    vector<int> targets;
+    cin >> t;
+    for(int i = 0; i < t; i++){
+        int size;
+        cin >> size;
+
+        vector<int> vec;
+        for(int j = 0; j < size; j++){
+            int value;
+            cin >> value;
+            vec.push_back(value);
+        }
+
+        int target;
+        cin >> target;
+
+        vecs.push_back(vec);
+        targets.push_back(target);
+    }
+
+    // solution
+    Solution s;
+    for(int i = 0; i < t; i++){
+        vector<int> result = s.twoSum(vecs[i], targets[i]);
+        cout << result[0] << " " << result[1] << endl;
+    }
+
+    return 0;
+}
